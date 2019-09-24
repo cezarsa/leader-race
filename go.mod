4c876d087279e46d3eaa19bace554de90e2dc036
Cezar Sa Espinola <cezarsa@gmail.com>
2019-09-24 14:05:34 -0300

    repro 1, old client-go

---
+++ go.mod
@@ -0,0 +1,26 @@
+module github.com/cezarsa/leader-race
+
+go 1.13
+
+require (
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/gogo/protobuf v1.3.0 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
+	github.com/golang/groupcache v0.0.0-20190702054246-869f871628b6 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/googleapis/gnostic v0.3.1 // indirect
+	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
+	github.com/json-iterator/go v1.1.7 // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20190923035154-9ee001bba392 // indirect
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
+	golang.org/x/time v0.0.0-20190921001708-c4c64cad1fd0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	k8s.io/api v0.0.0-20181004124137-fd83cbc87e76
+	k8s.io/apimachinery v0.0.0-20180913025736-6dd46049f395 // indirect
+	k8s.io/client-go v9.0.0+incompatible
+	k8s.io/kube-openapi v0.0.0-20190918143330-0270cf2f1c1d // indirect
+)
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"math/rand"
+	"time"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/leaderelection"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
+	"k8s.io/client-go/tools/record"
+)
+
+func main() {
+	client := fake.NewSimpleClientset()
+
+	broadcaster := record.NewBroadcaster()
+	recorder := broadcaster.NewRecorder(scheme.Scheme, v1.EventSource{
+		Component: "my-controller",
+	})
+	lock, err := resourcelock.New(
+		resourcelock.EndpointsResourceLock,
+		"default",
+		"my-controller",
+		client.CoreV1(),
+		// nil,
+		resourcelock.ResourceLockConfig{
+			Identity:      "host",
+			EventRecorder: recorder,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		ctx, cancel := context.WithCancel(context.Background())
+		le, err := leaderelection.NewLeaderElector(leaderelection.LeaderElectionConfig{
+			Lock:          lock,
+			LeaseDuration: 15 * time.Second,
+			RenewDeadline: 10 * time.Second,
+			RetryPeriod:   2 * time.Second,
+			Callbacks: leaderelection.LeaderCallbacks{
+				OnStartedLeading: func(ctx context.Context) {
+				},
+				OnStoppedLeading: func() {
+				},
+			},
+		})
+		if err != nil {
+			panic(err)
+		}
+		go func() {
+			time.Sleep((100 * time.Microsecond) + (time.Duration(rand.Intn(1000)) * time.Microsecond))
+			cancel()
+		}()
+		le.Run(ctx)
+	}
+}
